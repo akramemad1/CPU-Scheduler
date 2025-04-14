6d407bf7d01c9583253f4aff2d85507529c8c7a0
Kareem Wael Elhamy <143464378+7ekmaz@users.noreply.github.com>
2025-04-15 01:46:38 +0200

    Update srtf.py

--- core/schedulers/srtf.py
+++ core/schedulers/srtf.py
@@ -1,15 +1,15 @@
 import heapq
  
 class SRTFScheduler:
+    is_preemptive = True
 
-    def schedule(self, processes):
+    def schedule(self, processes, current_time):
         """
         Returns a list of dicts:
         Each dict contains: name, arrival, burst, duration, start
         """
         # Sort by arrival initially
         processes = sorted(processes, key=lambda p: p['arrival'])
-        time = 0
         i = 0
         ready_queue = []
         result = []
@@ -19,7 +19,7 @@ class SRTFScheduler:
         current_start = None
         while i < len(processes) or ready_queue or current_process:
             # Add all processes that arrive at this time
-            while i < len(processes) and processes[i]['arrival'] <= time:
+            while i < len(processes) and processes[i]['arrival'] <= current_time:
                 p = processes[i]
                 heapq.heappush(ready_queue, (remaining[p['name']], p['arrival'], p['name'], p))
                 i += 1
@@ -31,16 +31,17 @@ class SRTFScheduler:
                     result.append({
                         'name': current_process['name'],
                         'arrival': current_process['arrival'],
-                        'burst': remaining[current_process['name']] + (time - current_start),
+                        'burst': remaining[current_process['name']] + (current_time - current_start),
                         'start': current_start,
-                        'duration': time - current_start
+                        'duration': current_time - current_start
                     })
                     heapq.heappush(ready_queue, (remaining[current_process['name']], current_process['arrival'], current_process['name'], current_process))
                     current_process = None
+
             # If no process is running, pick the one with smallest remaining time
             if not current_process and ready_queue:
                 _, _, _, current_process = heapq.heappop(ready_queue)
-                current_start = time
+                current_start = current_time
             if current_process:
                 # Execute current process for 1 time unit
                 remaining[current_process['name']] -= 1
@@ -49,13 +50,14 @@ class SRTFScheduler:
                     result.append({
                         'name': current_process['name'],
                         'arrival': current_process['arrival'],
-                        'burst': remaining[current_process['name']] + (time - current_start + 1),
+                        'burst': remaining[current_process['name']] + (current_time - current_start + 1),
                         'start': current_start,
-                        'duration': time - current_start + 1
+                        'duration': current_time - current_start + 1
                     })
                     current_process = None
             # If nothing is running and nothing in queue, just idle forward
-            time += 1
+            current_time += 1
+            result.sort(key=lambda p: p['burst'] - p.get('ran', 0))
         return result
     
 '''
